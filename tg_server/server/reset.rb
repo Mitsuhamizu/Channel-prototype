71a367c91beda6e2c327736f200986d549171079
git <ZhichunLu@ZhiChun-Lus-MacBook-Pro.local>
2020-11-12 18:40:49 +0800

    minor

--- tg_server/server/reset.rb
+++ tg_server/server/reset.rb
@@ -2,10 +2,5 @@ require "mongo"
 Mongo::Logger.logger.level = Logger::FATAL
 @client = Mongo::Client.new(["127.0.0.1:27017"], :database => "GPC")
 @db = @client.database
-@db.drop()
-
-@path_to_cli = __dir__ + "/GPC.rb"
-@private_key_A = "0x63d86723e08f0f813a36ce6aa123bb2289d90680ae1e99d4de8cdb334553f24d"
-@private_key_B = "0x85ce75a6b678c6930a4f0938588f0240784971bb03632f1a2f1b25102b7cf5f0"
-system ("ruby " + @path_to_cli + " init #{@private_key_A}")
-system ("ruby " + @path_to_cli + " init #{@private_key_B}")
+@coll_sessions = @db["0x03babbec0930eb604d89520055f45d7e33e0f6c34e52a97e665c7fe52896044602_session_pool"]
+@coll_sessions.find_one_and_delete(id: "0459ff7946cf14a7db26d9f6f4095896")
